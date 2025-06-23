de25ea7f15e90720efd8dcca8718bb6a22d12249
Daniel Berthran <Berthlearn@gmail.com>
2025-06-23 14:34:11 +0100

    Create DTO for login client request and server response

--- src/main/java/com/nexusmart/api/dto/LoginResponseDTO.java
+++ src/main/java/com/nexusmart/api/dto/LoginResponseDTO.java
@@ -1,4 +1,12 @@
 package com.nexusmart.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor // A handy lombok annotation for a constructor with all fields
 public class LoginResponseDTO {
+    private String token;
 }
