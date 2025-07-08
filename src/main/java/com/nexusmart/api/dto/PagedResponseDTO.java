574c3fbb73a4493214308574ce184d3df3aabf12
Daniel Berthran <Berthlearn@gmail.com>
2025-07-08 13:42:30 +0100

    Create a custome ResponseDTO for paginated results

--- src/main/java/com/nexusmart/api/dto/PagedResponseDTO.java
+++ src/main/java/com/nexusmart/api/dto/PagedResponseDTO.java
@@ -1,4 +1,19 @@
 package com.nexusmart.api.dto;
 
-public class PagedResponseDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagedResponseDTO<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean isLast;
 }
